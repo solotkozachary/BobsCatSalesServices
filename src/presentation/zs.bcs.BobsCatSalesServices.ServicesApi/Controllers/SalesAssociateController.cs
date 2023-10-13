20a2e6d1b5e623e378ced077579baa99435fe914
Zachary Solotko <solotko.zachary@gmail.com>
2023-10-12 23:33:27 -0700

    wip

---
+++ src/presentation/zs.bcs.BobsCatSalesServices.ServicesApi/Controllers/SalesAssociateController.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Threading;
+using System.Threading.Tasks;
+using zs.bcs.BobsCatSalesServices.Application.SalesAssociate.Commands;
+using zs.bcs.BobsCatSalesServices.ServicesApi.Contracts.Requests;
+
+namespace zs.bcs.BobsCatSalesServices.ServicesApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SalesAssociateController : ControllerBase
+    {
+        private readonly ILogger<SalesAssociateController> _logger;
+        private readonly IMediator _mediator;
+        private readonly IMapper _mapper;
+
+        public SalesAssociateController(
+            ILogger<SalesAssociateController> logger,
+            IMediator mediator,
+            IMapper mapper
+            )
+        {
+            _logger = logger;
+            _mediator = mediator;
+            _mapper = mapper;
+        }
+
+        [HttpPost(Name = "CreateSalesAssociate")]
+        public async Task<string> CreateSalesAssociate(CreateSalesAssociateRequest request, CancellationToken cancellationToken)
+        {
+            _logger.LogTrace("Enter CreateSalesAssociate");
+
+            var command = _mapper.Map<CreateSalesAssociateCommand>(request);
+
+            var result = await _mediator.Send(command, cancellationToken);
+
+            _logger.LogTrace("Exit CreateSalesAssociate - SalesAssociateEntityKey:{SalesAssociateEntityKey}", result);
+
+            return result;
+        }
+
+    }
+}
---
+++ src/presentation/zs.bcs.BobsCatSalesServices.ServicesApi/Mapping/MappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using zs.bcs.BobsCatSalesServices.Application.SalesAssociate.Commands;
+using zs.bcs.BobsCatSalesServices.ServicesApi.Contracts.Requests;
+
+namespace zs.bcs.BobsCatSalesServices.ServicesApi.Mapping
+{
+    /// <summary>
+    /// AutoMapper mapping profile.
+    /// </summary>
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<CreateSalesAssociateRequest, CreateSalesAssociateCommand>();
+        }
+    }
+}
